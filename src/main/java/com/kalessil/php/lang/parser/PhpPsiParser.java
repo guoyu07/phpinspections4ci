4ee0dd93706de96d535a24bf75047bc2477011b8
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-30 17:02:25 +0200

    PhpPsiParser: fixing

--- src/main/java/com/kalessil/php/lang/parser/PhpPsiParser.java
+++ src/main/java/com/kalessil/php/lang/parser/PhpPsiParser.java
@@ -1,36 +1,31 @@
 package com.kalessil.php.lang.parser;
 
-import com.kalessil.php.lang.parser.parsing.Program;
-import com.kalessil.php.lang.parser.util.PhpPsiBuilder;
-import org.jetbrains.annotations.NotNull;
 import com.intellij.lang.ASTNode;
-import consulo.lang.LanguageVersion;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.PsiParser;
 import com.intellij.psi.tree.IElementType;
+import com.kalessil.php.lang.parser.parsing.Program;
+import com.kalessil.php.lang.parser.util.PhpPsiBuilder;
+import org.jetbrains.annotations.NotNull;
 
 /**
- * Created by IntelliJ IDEA.
- * User: jay
- * Date: 26.02.2007
- *
  * @author jay
+ * @author kalessil
  */
 public class PhpPsiParser implements PsiParser
 {
+	PhpPsiParser() {
+    }
 
-	@Override
-	@NotNull
-	public ASTNode parse(IElementType root, PsiBuilder builder, LanguageVersion languageVersion)
-	{
-		builder.setDebugMode(true);
-		PhpPsiBuilder psiBuilder = new PhpPsiBuilder(builder);
-
-		PsiBuilder.Marker marker = psiBuilder.mark();
-		//    long startTime = System.currentTimeMillis();
-		Program.parse(psiBuilder);
-		//    System.out.println("parsing time: " + (System.currentTimeMillis() - startTime));
-		marker.done(root);
+    @NotNull
+    public ASTNode parse(@NotNull IElementType root, @NotNull PsiBuilder builder) {
+		// builder.setDebugMode(true);
+        PhpPsiBuilder psiBuilder = new PhpPsiBuilder(builder);
+        PsiBuilder.Marker marker = psiBuilder.mark();
+        // long startTime = System.currentTimeMillis();
+        Program.parse(psiBuilder);
+        // System.out.println("parsing time: " + (System.currentTimeMillis() - startTime));
+        marker.done(root);
 		return psiBuilder.getTreeBuilt();
-	}
+    }
 }
