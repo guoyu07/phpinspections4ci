603efa8dff091496b571737a4453434f9618d33e
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-30 17:13:43 +0200

    PhpSyntaxHighlighter: fixing

--- src/main/java/com/kalessil/php/lang/highlighter/PhpSyntaxHighlighter.java
+++ src/main/java/com/kalessil/php/lang/highlighter/PhpSyntaxHighlighter.java
@@ -1,32 +1,53 @@
 package com.kalessil.php.lang.highlighter;
 
-import com.kalessil.php.lang.PhpLanguage;
-import com.kalessil.php.lang.lexer.PhpTokenTypes;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import com.intellij.ide.highlighter.HtmlFileType;
+import com.intellij.lexer.Lexer;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.editor.ex.util.LayerDescriptor;
 import com.intellij.openapi.editor.ex.util.LayeredLexerEditorHighlighter;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypes;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import consulo.lang.LanguageVersionResolvers;
+import com.intellij.psi.tree.IElementType;
+import com.kalessil.php.lang.lexer.PhpTokenTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
- * Created by IntelliJ IDEA.
- * User: jay
- * Date: 22.02.2007
- *
  * @author jay
+ * @author kalessil
  */
 public class PhpSyntaxHighlighter extends LayeredLexerEditorHighlighter
 {
-	public PhpSyntaxHighlighter(@Nullable final Project project, @Nullable final VirtualFile virtualFile, @NotNull final EditorColorsScheme colors)
-	{
-		super(new PhpFileSyntaxHighlighter(LanguageVersionResolvers.INSTANCE.forLanguage(PhpLanguage.INSTANCE).getLanguageVersion(PhpLanguage
-				.INSTANCE, project, virtualFile)), colors);
-		registerLayer(PhpTokenTypes.HTML, new LayerDescriptor(SyntaxHighlighterFactory.getSyntaxHighlighter(HtmlFileType.INSTANCE, project,
-				virtualFile), ""));
-	}
+	public PhpSyntaxHighlighter(@Nullable Project project, @Nullable VirtualFile virtualFile, @NotNull EditorColorsScheme colors) {
+        this(project, virtualFile, colors, null);
+    }
+
+    public PhpSyntaxHighlighter(
+    		@Nullable Project project,
+			@Nullable VirtualFile virtualFile,
+			@NotNull EditorColorsScheme colors,
+			@Nullable Lexer baseLexer
+	) {
+        super(new PhpFileSyntaxHighlighter(baseLexer), colors);
+
+        final FileType fileType             = virtualFile == null ? FileTypes.PLAIN_TEXT : virtualFile.getFileType();
+        final SyntaxHighlighter highlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(fileType, project, virtualFile);
+        if (highlighter != null) {
+			this.registerLayer(PhpTokenTypes.HTML, new LayerDescriptor(new SyntaxHighlighter() {
+				@NotNull
+				public Lexer getHighlightingLexer() {
+					return highlighter.getHighlightingLexer();
+				}
+
+				@NotNull
+				public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
+					return highlighter.getTokenHighlights(tokenType);
+				}
+			}, ""));
+		}
+    }
 }
