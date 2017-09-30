682d46bf2cae0507b42392a1739b04f2265482ab
Vladimir Reznichenko <kalessil@gmail.com>
2017-09-30 16:51:29 +0200

    PhpStubElement: fixing

--- src/main/java/com/kalessil/php/lang/psi/impl/stub/elements/PhpStubElement.java
+++ src/main/java/com/kalessil/php/lang/psi/impl/stub/elements/PhpStubElement.java
@@ -7,21 +7,18 @@ import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.stubs.IStubElementType;
 import com.intellij.psi.stubs.StubElement;
-import consulo.psi.tree.IElementTypeAsPsiFactory;
 
 /**
  * @author VISTALL
  * @since 16.07.13.
  */
-public abstract class PhpStubElement<StubT extends StubElement, PsiT extends PsiElement> extends IStubElementType<StubT,
-		PsiT> implements IElementTypeAsPsiFactory
+public abstract class PhpStubElement<StubT extends StubElement, PsiT extends PsiElement> extends IStubElementType<StubT, PsiT>
 {
 	public PhpStubElement(@NotNull @NonNls String debugName)
 	{
 		super(debugName, PhpLanguage.INSTANCE);
 	}
 
-	@Override
 	@NotNull
 	public abstract PsiT createElement(@NotNull ASTNode node);
 
