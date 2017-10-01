04e8808de229413f7f0ab10f85d0b1c3405d17bf
Vladimir Reznichenko <kalessil@gmail.com>
2017-10-01 07:25:29 +0200

    OpenApi visitor/inspection adapted: migrated

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/openApi/BasePhpElementVisitor.java
@@ -0,0 +1,6 @@
+package com.kalessil.phpStorm.phpInspectionsEA.openApi;
+
+import com.kalessil.php.lang.psi.visitors.PhpElementVisitor;
+
+public abstract class BasePhpElementVisitor extends PhpElementVisitor {
+}
---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/openApi/BasePhpInspection.java
@@ -0,0 +1,6 @@
+package com.kalessil.phpStorm.phpInspectionsEA.openApi;
+
+import com.kalessil.php.lang.inspections.PhpInspection;
+
+public abstract class BasePhpInspection extends PhpInspection {
+}
