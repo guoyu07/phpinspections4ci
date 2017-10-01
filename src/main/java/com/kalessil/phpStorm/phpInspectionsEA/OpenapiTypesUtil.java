32e8a02c9d0739f01defa6c150e4c3a7cd394b7c
Vladimir Reznichenko <kalessil@gmail.com>
2017-10-01 07:19:56 +0200

    OpenapiTypesUtil: migrated

---
+++ src/main/java/com/kalessil/phpStorm/phpInspectionsEA/OpenapiTypesUtil.java
@@ -0,0 +1,35 @@
+package com.kalessil.phpStorm.phpInspectionsEA.utils;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
+import com.kalessil.php.lang.parser.PhpElementTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+final public class OpenapiTypesUtil {
+
+    static public boolean isAssignment(@Nullable PsiElement expression) {
+        return expression != null && expression.getNode().getElementType() == PhpElementTypes.ASSIGNMENT_EXPRESSION;
+    }
+
+    static public boolean isFunctionReference(@Nullable PsiElement expression) {
+        return expression != null && expression.getNode().getElementType() == PhpElementTypes.FUNCTION_CALL;
+    }
+
+    static public boolean isStatementImpl(@Nullable PsiElement expression) {
+        return expression != null && expression.getNode().getElementType() == PhpElementTypes.STATEMENT;
+    }
+
+    static public boolean isPhpExpressionImpl(@Nullable PsiElement expression) {
+        return expression != null && expression.getNode().getElementType() == PhpElementTypes.EXPRESSION;
+    }
+
+    static public boolean isNumber(@Nullable PsiElement expression) {
+        return expression != null && expression.getNode().getElementType() == PhpElementTypes.NUMBER;
+    }
+
+    static public boolean is(@Nullable PsiElement expression, @NotNull IElementType type) {
+        return expression != null && expression.getNode().getElementType() == type;
+    }
+}
