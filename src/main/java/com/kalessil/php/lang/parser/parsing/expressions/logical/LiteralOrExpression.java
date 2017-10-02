03170cfdf525ec2f24ad92b8a75c8cb0ec684d86
Vladimir Reznichenko <kalessil@gmail.com>
2017-10-02 09:18:19 +0200

    StatementList: code grooming

--- src/main/java/com/kalessil/php/lang/parser/parsing/expressions/logical/LiteralOrExpression.java
+++ src/main/java/com/kalessil/php/lang/parser/parsing/expressions/logical/LiteralOrExpression.java
@@ -8,67 +8,55 @@ import com.intellij.lang.PsiBuilder;
 import com.intellij.psi.tree.IElementType;
 
 /**
- * Created by IntelliJ IDEA.
- * User: markov
- * Date: 14.12.2007
+ * @author markov
+ * @author kalessil
  */
 public class LiteralOrExpression implements PhpTokenTypes
 {
 
 	public static IElementType parse(PhpPsiBuilder builder)
 	{
-		PsiBuilder.Marker marker = builder.mark();
-		IElementType result = LiteralXorExpression.parse(builder);
-		if(result != PhpElementTypes.EMPTY_INPUT)
-		{
-			if(builder.compareAndEat(opLIT_OR))
-			{
-				result = LiteralXorExpression.parse(builder);
-				if(result == PhpElementTypes.EMPTY_INPUT)
-				{
+        PsiBuilder.Marker marker = builder.mark();
+        IElementType result = LiteralXorExpression.parse(builder);
+        if (result != PhpElementTypes.EMPTY_INPUT) {
+            if (builder.compareAndEat(PhpTokenTypes.opLIT_OR)) {
+                result = LiteralXorExpression.parse(builder);
+                if (result == PhpElementTypes.EMPTY_INPUT) {
 					builder.error(PhpParserErrors.expected("expression"));
-				}
-				PsiBuilder.Marker newMarker = marker.precede();
-				marker.done(PhpElementTypes.LITERAL_LOGICAL_EXPRESSION);
-				result = PhpElementTypes.LITERAL_LOGICAL_EXPRESSION;
-				if(builder.compareAndEat(opLIT_OR))
-				{
-					subParse(builder, newMarker);
-				}
-				else
-				{
-					newMarker.drop();
-				}
-			}
-			else
-			{
-				marker.drop();
-			}
-		}
-		else
-		{
-			marker.drop();
-		}
-		return result;
+                }
+
+                PsiBuilder.Marker newMarker = marker.precede();
+                marker.done(PhpElementTypes.LITERAL_LOGICAL_EXPRESSION);
+                result = PhpElementTypes.LITERAL_LOGICAL_EXPRESSION;
+                if (builder.compareAndEat(PhpTokenTypes.opLIT_OR)) {
+                    subParse(builder, newMarker);
+                } else {
+                    newMarker.drop();
+                }
+            } else {
+                marker.drop();
+            }
+        } else {
+            marker.drop();
+        }
+
+        return result;
 	}
 
 	private static IElementType subParse(PhpPsiBuilder builder, PsiBuilder.Marker marker)
 	{
-		if(LiteralXorExpression.parse(builder) == PhpElementTypes.EMPTY_INPUT)
-		{
+        if (LiteralXorExpression.parse(builder) == PhpElementTypes.EMPTY_INPUT) {
 			builder.error(PhpParserErrors.expected("expression"));
-		}
-		PsiBuilder.Marker newMarker = marker.precede();
-		marker.done(PhpElementTypes.LITERAL_LOGICAL_EXPRESSION);
-		if(builder.compareAndEat(opLIT_OR))
-		{
-			subParse(builder, newMarker);
-		}
-		else
-		{
-			newMarker.drop();
-		}
-		return PhpElementTypes.LITERAL_LOGICAL_EXPRESSION;
-	}
+        }
 
+        PsiBuilder.Marker newMarker = marker.precede();
+        marker.done(PhpElementTypes.LITERAL_LOGICAL_EXPRESSION);
+        if (builder.compareAndEat(PhpTokenTypes.opLIT_OR)) {
+            subParse(builder, newMarker);
+        } else {
+            newMarker.drop();
+        }
+
+        return PhpElementTypes.LITERAL_LOGICAL_EXPRESSION;
+	}
 }
